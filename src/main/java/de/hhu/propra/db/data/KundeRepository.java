45b95851ae81cdaa8a8d7c7a62be4d23befd18cd
konietse <koniet.se@gmail.com>
2018-11-15 15:29:20 +0100

    force push

--- src/main/java/de/hhu/propra/db/data/KundeRepository.java
+++ src/main/java/de/hhu/propra/db/data/KundeRepository.java
@@ -3,6 +3,10 @@ package de.hhu.propra.db.data;
 import de.hhu.propra.db.entities.Kunde;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface KundeRepository extends CrudRepository<Kunde, Long> {
+
+    List<Kunde> findAll();
 
 }
--- src/main/java/de/hhu/propra/db/entities/Kunde.java
+++ src/main/java/de/hhu/propra/db/entities/Kunde.java
@@ -2,10 +2,8 @@ package de.hhu.propra.db.entities;
 
 import lombok.Data;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.List;
 
 @Data // Lombok - Generiert Getter, Setter, toString, ...
 @Entity
@@ -19,4 +17,7 @@ public class Kunde {
     private Integer plz;
 
     // TODO: Hier muss irgendetwas mit Autos hin...
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    private List<Auto> autos;
+
 }
